61d0cebb44a783c5a58aa92e7e2309a3e7bcf3e4
Ticiana Capris <tcapris@hotmail.com>
2022-09-14 19:16:06 +0100

    Create Routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,25 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
   return response.json([
     { id: "1", name: "Ad 1" },
     { id: "2", name: "Ad 2" },
     { id: "3", name: "Ad 3" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  return response.json([
+
   ]);
 });
 
